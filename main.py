8a909f4f10c5a35d7cd5adbe28bb974a44176902
Lucas Henrique Pecazevicz Pancioni <lucaspecazevicz@gmail.com>
2025-05-19 03:40:48 +0000

    limpeza Base/engine

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
+# main.py
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from backend.database import Base, engine
-from backend.routers import montadoras, modelos, pecas
-from frontend.dash_app import create_dash_app
-from starlette.middleware.wsgi import WSGIMiddleware
+from backend.routers import montadoras
 
 app = FastAPI()
 
-# Permitir frontend acessar a API
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-Base.metadata.create_all(bind=engine)
-
-# Backend
+# Incluir rotas
 app.include_router(montadoras.router)
-app.include_router(modelos.router)
-app.include_router(pecas.router)
-
-# Frontend (Dash)
-app.mount("/", WSGIMiddleware(create_dash_app()))
